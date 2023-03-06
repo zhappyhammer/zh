8a2191182384618812e5fc0559f2346590e4d515
zhappy <zkxtech@outlook.com>
2023-03-06 09:34:59 +0800

    2023-03-06 09:34:59

--- deployzh.sh
+++ deployzh.sh
@@ -1,6 +1,10 @@
 message=$(date "+%Y-%m-%d %H:%M:%S")
+
 git add .
 git commit -m "$message"
+
 git fetch hhzh main && git merge hhzh FETCH_HEAD
-git push -u hhzh main
-mkdocs gh-deploy
+git push hhzh main
+#git push -u hhzh main
+
+#mkdocs gh-deploy --force
